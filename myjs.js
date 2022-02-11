4fe8e69f9c450ccb8f97f2cfe00342c18937c310
Alvin Alvrahesta <alvin.alvrahesta@gmail.com>
2022-02-11 16:57:48 +0700

    Back to Top

--- myjs.js
+++ myjs.js
@@ -1,3 +1,28 @@
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: "#navbar-example2",
 });
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+    mybutton.style.transition = "0.5s";
+  } else {
+    mybutton.style.display = "none";
+    mybutton.style.transition = "0.5s";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
