16191db0ee7bf829adda89d713d77776f8abd510
Alvin Alvrahesta <alvin.alvrahesta@gmail.com>
2022-02-23 23:47:55 +0700

    Navbar Mobile

--- myjs.js
+++ myjs.js
@@ -3,6 +3,7 @@ var scrollSpy = new bootstrap.ScrollSpy(document.body, {
 });
 //Get the button
 let mybutton = document.getElementById("btn-back-to-top");
+let gototop = document.getElementById("gototop");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
@@ -10,12 +11,16 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     mybutton.style.display = "block";
     mybutton.style.transition = "0.5s";
+    gototop.style.opacity = "1";
+    gototop.style.transition = "0.5s";
   } else {
     mybutton.style.display = "none";
     mybutton.style.transition = "0.5s";
+    gototop.style.opacity = "0";
+    gototop.style.transition = "0.5s";
   }
 }
 // When the user clicks on the button, scroll to the top of the document
