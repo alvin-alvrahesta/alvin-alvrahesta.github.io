b5626a1b7092564ff3bf2b4113e9af854807f35f
Alvin Alvrahesta <alvin.alvrahesta@gmail.com>
2023-08-16 17:28:11 +0700

    Revert "Coba ganti pakai Tailwind"
    This reverts commit f079e6f57920d5eb5b11cdbac3f18a5ae514b9e8.

---
+++ myjs.js
@@ -0,0 +1,38 @@
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: "#navbar-example2",
+});
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: "#navbar-example1",
+});
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+let gototop = document.getElementById("gototop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+    mybutton.style.transition = "0.5s";
+  } else {
+    mybutton.style.display = "none";
+    mybutton.style.transition = "0.5s";
+  }
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    gototop.style.opacity = "1";
+    gototop.style.transition = "0.5s";
+  } else {
+    gototop.style.opacity = "0";
+    gototop.style.transition = "0.5s";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
